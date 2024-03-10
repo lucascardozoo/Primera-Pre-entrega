b5f72d84c8e24bd1cb82efa970ebefd6948348f1
Lucas Cardozo <contactocardozolucas@gmail.com>
2024-03-10 01:39:40 -0300

    Asincronismo y peticiones

--- js/script.js
+++ js/script.js
@@ -1,84 +1,5 @@
-//Array de objetos para renderizar los productos que se pueden agregar la carrito
-const products = [
-    {
-        id: 1,
-        imagen: "procesador.jpg",
-        nombre: "Procesador Intel i5 14400F",
-        precio: 319900
-    },
-    {
-        id: 2,
-        imagen: "mother.jpg",
-        nombre: "Mother ASUS TUF GAMING B660M-PLUS",
-        precio: 213750
-    },
-    {
-        id: 3,
-        imagen: "placa-de-video.jpg",
-        nombre: "Placa de Video MSI GeForce RTX 4080 16GB",
-        precio: 1648710
-    },
-    {
-        id: 4,
-        imagen: "ram.jpg",
-        nombre: "Memoria Team DDR4 16GB (2x8GB) 3200MHz",
-        precio: 62300
-    },
-    {
-        id: 5,
-        imagen: "disco-m.2.jpg",
-        nombre: "Disco SSD M.2 500GB WD_Black 5000MB/s NVMe",
-        precio: 69990
-    },
-    {
-        id: 6,
-        imagen: "disco-ssd.jpg",
-        nombre: "Disco Sólido SSD Kingston 960GB A400 500MB/s",
-        precio: 71700
-    },
-    {
-        id: 7,
-        imagen: "cooler-cpu.jpg",
-        nombre: "Cooler CPU Cooler Master ML360 Water Cooling",
-        precio: 235859
-    },
-    {
-        id: 8,
-        imagen: "gabinete.jpg",
-        nombre: "Gabinete Cooler Master HAF 700",
-        precio: 377900
-    },
-    {
-        id: 9,
-        imagen: "fuente.jpg",
-        nombre: "Fuente Corsair 750W 80 Plus Gold RM750X Full Modular",
-        precio: 151990
-    },
-    {
-        id: 10,
-        imagen: "monitor.jpg",
-        nombre: "Monitor Gamer AOC 27¨",
-        precio: 335000
-    },
-    {
-        id: 11,
-        imagen: "auriculares.jpg",
-        nombre: "Auriculares HyperX Cloud Flight Black Wireless",
-        precio: 113000
-    },
-    {
-        id: 12,
-        imagen: "teclado.jpg",
-        nombre: "Teclado Mecanico HyperX Alloy Elite 2 Switch Red LA",
-        precio: 141250
-    },
-    {
-        id: 13,
-        imagen: "mouse.jpg",
-        nombre: "Mouse Logitech G502 Gaming Hero",
-        precio: 79900
-    },
-]
+//Archivo JSON con el array de objetos para renderizar los productos que se pueden agregar la carrito
+const rutaJson = '../data/products-tienda.json';
 
 //Para instanciar los productos que se agregan al carrito
 class product {
@@ -121,6 +42,21 @@ function recuperoNombreDeLS () {
 }
 recuperoNombreDeLS();
 
+//Función para hacer la petición al archivo JSON y retorna el array de objetos de los productos o un error
+async function peticion (rutaJson) {
+    try {
+        const response = await fetch(rutaJson);
+        if (!response.ok) {
+            throw new Error ( `Error de petición: ${response.status} ${response.statusText}` )
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err)
+    }
+}
+
 //Para guardar en el Local Storage
 function saveProductLocalStorage () {
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -133,8 +69,10 @@ function sumaCantidadCart () {
 }
 
 //Botones para agregar al carrito o sumar uno a la cantidad del producto si ya existe y guardar en el Local Storage
-function addToCardButton () {
+async function addToCardButton () {
     const addButton = document.querySelectorAll(".btnAgregarCarrito");
+    const products = await peticion(rutaJson); 
+
     addButton.forEach(button => {
         button.onclick = (e) => {
             const productId = e.target.id;
@@ -160,8 +98,10 @@ function addToCardButton () {
 }
 
 //Para renderizar productos que se pueden agregar al carrito, renderiza el array de objetos "products" creado con anteriroidad
-function renderProducts () {
-    products.forEach( product => {
+async function renderProducts () {
+    const productos = await peticion(rutaJson); 
+
+    productos.forEach( product => {
         const article = document.createElement("article");
         article.className = "product";
         article.innerHTML = `
